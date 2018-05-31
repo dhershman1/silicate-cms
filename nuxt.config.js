1bf64b9113feca9d09abd92c6fd7979d3e7c4fc7
dhershman1 <dustinh17@gmail.com>
2018-05-31 16:50:34 -0400

    add lang to html tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ module.exports = {
   */
   head: {
     title: 'Silicate CMS',
+    htmlAttrs: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
