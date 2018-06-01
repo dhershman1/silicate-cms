b46535655839ca0ab51c0c862696cddf27bb3c5c
dhershman1 <dustinh17@gmail.com>
2018-05-31 21:12:22 -0400

    Started working on side navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div role="main">
+    <ul class="sidenav sidenav-fixed">
+      <li>
+        <a class="black-text" href="/dashboard"><i class="material-icons">dashboard</i>Dashboard</a>
+      </li>
+      <li>
+        <a class="black-text" href="/users"><i class="material-icons">person</i>Users</a>
+      </li>
+      <li>
+        <a class="black-text" href="/"><i class="material-icons">lock</i>Login</a>
+      </li>
+    </ul>
     <nuxt/>
   </div>
 </template>
