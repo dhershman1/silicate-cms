7a03f7eb98daf90afc1a36e920846b4ebc983957
dhershman1 <dustinh17@gmail.com>
2018-06-01 22:24:38 -0400

    Replaced material icons with Raleway font

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,19 +9,21 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'simple cms system built on nuxt & node' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'simple cms system built on nuxt & node'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/imgs/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+        href: 'https://fonts.googleapis.com/css?family=Raleway'
       }
     ]
   },
-  css: [
-    '@/static/css/main.css'
-  ],
+  css: ['@/static/css/font-face.css', '@/static/css/main.css'],
   /*
   ** Customize the progress bar color
   */
